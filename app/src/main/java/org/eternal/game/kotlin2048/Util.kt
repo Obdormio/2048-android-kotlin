ff6e72fc04a98a91bb4aa18e9027c66076584ae8
Eternal-A <15828377403@163.com>
2018-12-05 16:37:29 +0800

    add util: convert sp to px

---
+++ app/src/main/java/org/eternal/game/kotlin2048/Util.kt
@@ -0,0 +1,12 @@
+package org.eternal.game.kotlin2048
+
+import android.content.Context
+import android.util.TypedValue
+
+fun Context.sp2px(spSize: Float): Float {
+    return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            spSize,
+            resources.displayMetrics
+    )
+}
